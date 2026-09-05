81ea4da79298c2f78c9864ee8158a35457b9d948
freze2212 <frezeit@kjc.com>
2026-09-05 19:07:17 +0700

    Update link & telegram for domain gg88am.com

--- config.js
+++ config.js
@@ -2,11 +2,17 @@
  * Cấu hình link redirect theo domain (Cloudflare Pages).
  */
 window.LINK_CONFIG = {
-  default: "https://gg8835.com/?id=153189538",
+  default: "https://gg8817.com/?id=340055393",
 
   domains: {
+    "www.www.gg88am.com": "https://gg8817.com/?id=340055393",
+    "www.gg88am.com": "https://gg8817.com/?id=340055393",
+    "gg8sing.com": "https://www.gg8838.com/?id=407699684",
+    "www.gg8sing.com": "https://www.gg8838.com/?id=407699684",
     "www.gg88sing.co": "https://www.gg8824.com/?id=516358228",
+    "gg88sing.co": "https://www.gg8824.com/?id=516358228",
     "www.gg8sing.vip": "https://gg8824.com/?id=966041804",
+    "gg8sing.vip": "https://gg8824.com/?id=966041804",
     "ggtong.vip": "https://gg8842.com/?id=934472881",
     "gg8869.net": "https://gg8835.com/?id=647827737",
     "gg8888.uk": "https://gg8824.com/?id=582517684",
@@ -21,14 +27,11 @@ window.LINK_CONFIG = {
     "gg88bet.uk": "https://www.gg8850.com/?id=532307045",
     "g8d1.net": "https://www.gg8859.com/?id=235748786",
     "gg88qte.com": "https://gg8846.com/?id=566308881",
-    "gg88sing.co": "https://www.gg8824.com/?id=516358228",
-    "g88sing.com": "https://www.gg8838.com/?id=407699684",
     "gg88sin.net": "https://www.gg8838.com/?id=407699684",
     "gg88sgp.com": "https://www.gg8826.com/?id=720056733",
     "gg88quocte.com": "https://www.gg8826.com/?id=720056733",
     "gg88sing.org": "https://www.gg8826.com/?id=720056733",
-    "gg8sing.vip": "https://gg8824.com/?id=966041804",
-    "gg88am.com": "https://www.gg8826.com/?id=720056733",
+    "gg88am.com": "https://gg8817.com/?id=340055393",
   },
 };
 
@@ -43,6 +46,10 @@ window.LINK_CONFIG = {
     var cfg = window.LINK_CONFIG || {};
     var domains = cfg.domains || {};
     var host = normalizeHost(window.location.hostname);
+    var fullHost = (window.location.hostname || "").toLowerCase();
+
+    if (domains[fullHost]) return domains[fullHost];
+    if (domains[host]) return domains[host];
 
     for (var domain in domains) {
       if (!Object.prototype.hasOwnProperty.call(domains, domain)) continue;
@@ -55,4 +62,27 @@ window.LINK_CONFIG = {
   };
 
   window.REDIRECT_URL = window.getRedirectUrl();
+
+  // Tự động đồng bộ domains.json thời gian thực
+  try {
+    fetch('/domains.json')
+      .then(function(res) { return res.json(); })
+      .then(function(dj) {
+        if (!dj) return;
+        var h = (window.location.hostname || "").toLowerCase();
+        var normH = normalizeHost(h);
+        var entry = dj[h] || dj[normH] || dj['www.' + normH];
+        if (entry) {
+          var target = entry.main_url || entry.url || entry.link || (typeof entry === 'string' ? entry : '');
+          if (target) {
+            window.REDIRECT_URL = target;
+            var links = document.querySelectorAll('a.redirect-link');
+            for (var i = 0; i < links.length; i++) {
+              links[i].href = target;
+            }
+          }
+        }
+      })
+      .catch(function() {});
+  } catch(e) {}
 })();
